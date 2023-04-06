97ff63d128f6c9c366b93b6df38f74ef7583c63c
EricXieQ <97911585+EricXieQ@users.noreply.github.com>
2023-04-06 03:08:50 -0400

    woo

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import {
   Tooltip
 } from "recharts";
 
+import {LineChart, Line ,Legend} from "recharts"
+
 function App() {
 
   const [apiData, setData] = useState(null)
@@ -115,6 +117,25 @@ function App() {
             <Tooltip />
             <Area type="monotone" dataKey="temperature" stroke="#8884d8" fill="#8884d8" />
           </AreaChart>
+
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="temperature" stroke="#82ca9d" />
+        </LineChart>
       </div>
       </div>
     </div>
